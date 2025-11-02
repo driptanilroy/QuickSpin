e3e628811c46a52ef23f7eb514f5269dc61574cd
Driptanil Roy <153745351+driptanilroy@users.noreply.github.com>
2025-11-02 05:27:36 +0000

    vue tailwind axios added

---
+++ quickspin.sh
@@ -0,0 +1,16 @@
+echo "Enter project name : "
+read name
+
+echo "Creating Vite app: $name"
+
+npm create vite@latest $name -- --template vue --yes && wait
+
+cd $name
+
+echo "Installing  packages ..."
+
+npm i && npm i tailwindcss axios
+
+echo "don't forget to configure tailwind"
+
+npm i -D prettier
